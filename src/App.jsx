2214c785b9d586c8f7b676c0077846e2e6e914b3
IALTAIR <yferoze@gmail.com>
2026-01-18 05:18:07 +0100

    ajout fichier book list

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
-import React, { useEffect, useState } from 'react';
-import {
-  BrowserRouter, Route, Routes,
-} from 'react-router-dom';
-import SignIn from './pages/SignIn/SignIn';
-import Home from './pages/Home/Home';
-import Book from './pages/Book/Book';
-import { APP_ROUTES } from './utils/constants';
-import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
-import AddBook from './pages/AddBook/AddBook';
-import UpdateBook from './pages/updateBook/UpdateBook';
-import { useUser } from './lib/customHooks';
-import ScrollToTop from './components/ScrollToTop/ScrollToTop';
+import React, { useEffect, useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import SignIn from "./pages/SignIn/SignIn";
+import Home from "./pages/Home/Home";
+import Book from "./pages/Book/Book";
+import { APP_ROUTES } from "./utils/constants";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import AddBook from "./pages/AddBook/AddBook";
+import UpdateBook from "./pages/updateBook/UpdateBook";
+import { useUser } from "./lib/customHooks";
+import ScrollToTop from "./components/ScrollToTop/ScrollToTop";
+import BooksList from "./components/BooksList";
 
 function App() {
   const [user, setUser] = useState(null);
@@ -25,9 +24,13 @@ function App() {
       <div>
         <ScrollToTop />
         <Header user={user} setUser={setUser} />
+        <BooksList />
         <Routes>
           <Route index element={<Home />} />
-          <Route path={APP_ROUTES.SIGN_IN} element={<SignIn setUser={setUser} />} />
+          <Route
+            path={APP_ROUTES.SIGN_IN}
+            element={<SignIn setUser={setUser} />}
+          />
           <Route path={APP_ROUTES.BOOK} element={<Book />} />
           <Route path={APP_ROUTES.UPDATE_BOOK} element={<UpdateBook />} />
           <Route path={APP_ROUTES.ADD_BOOK} element={<AddBook />} />
---
+++ src/components/BooksList.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from "react";
+
+function BooksList() {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    fetch("https://mon-vieux-grimoire-backend-lmkk.onrender.com/")
+      .then(res => res.json())
+      .then(data => setBooks(data))
+      .catch(err => console.error("Erreur API :", err));
+  }, []);
+
+  return (
+    <div>
+      <h2>Liste des livres</h2>
+      <ul>
+        {books.map(book => (
+          <li key={book.id}>{book.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default BooksList;
